fbcce14ad20286e4adf6613fa360ea97a7babe46
Bill Wright <bill@wwwright.com>
2025-03-07 15:18:16 -0700

    Reorganizing image files in preparation of using character images

--- src/main/java/csci/ooad/layout/alternate/AlternateMazeObserver.java
+++ src/main/java/csci/ooad/layout/alternate/AlternateMazeObserver.java
@@ -5,11 +5,22 @@ import csci.ooad.layout.intf.IMazeObserver;
 
 import java.util.List;
 
+/**
+ * This class represents an alternative observer for a maze,
+ * The observer gets updates about the maze and its status.
+ */
 public class AlternateMazeObserver implements IMazeObserver {
 
     static {
         System.out.println("AlternateMazeObserver class is being loaded");
     }
+
+    /**
+     * This method updates the observer about the maze status, prints status messages, the room names, and their contents.
+     *
+     * @param maze the maze object that is to be observed.
+     * @param statusMessages a list of status messages (String) about the maze.
+     */
     @Override
     public void update(IMaze maze, List<String> statusMessages) {
         for (String msg : statusMessages) {
--- src/main/java/csci/ooad/layout/intf/IMazeObserver.java
+++ src/main/java/csci/ooad/layout/intf/IMazeObserver.java
@@ -3,15 +3,43 @@ package csci.ooad.layout.intf;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * This interface represents an Observer of a Maze, notified for updates in the Maze state.
+ */
 public interface IMazeObserver {
+
+    /**
+     * Updates the specified maze.
+     *
+     * @param maze the maze to be updated
+     */
     default void update(IMaze maze) {
         update(maze, "");
     }
+
+    /**
+     * Updates the specified maze with the given status message.
+     *
+     * @param maze the maze to be updated
+     * @param statusMessages the status message to be displayed
+     */
     default void update(IMaze maze, String statusMessages) {
         update(maze, Collections.singletonList(statusMessages));
     }
+
+    /**
+     * Updates the specified maze with the given status messages.
+     *
+     * @param maze the maze to be updated
+     * @param statusMessages the status messages to be displayed
+     */
     void update(IMaze maze, List<String> statusMessages);
 
+    /**
+     * Paints the maze to a file.
+     *
+     * @param filePath the file path to paint the maze to
+     */
     default void paintToFile(String filePath) {
     }
 }
