34fbaa151acf4290d0aef11154459432a3f510e4
William Warren Wright <143556583+BillWrightCUProf@users.noreply.github.com>
2026-04-18 17:09:47 -0600

    Added EventType and an attach method for specified types to the IGame interface.

---
+++ GameObserverSPI/src/main/java/ooad/gameobserver/intf/EventType.java
@@ -0,0 +1,12 @@
+package ooad.gameobserver.intf;
+
+public enum EventType {
+    TREASURE_FOUND,
+    FIGHT_OCCURRED,
+    CHARACTER_DIED,
+    CHARACTER_MOVED,
+    CHARACTER_ATE,
+    CHARACTER_DRANK,
+    ADD_CHARACTER,
+    ALL
+}
--- GameObserverSPI/src/main/java/ooad/gameobserver/intf/IGame.java
+++ GameObserverSPI/src/main/java/ooad/gameobserver/intf/IGame.java
@@ -1,8 +1,13 @@
 package ooad.gameobserver.intf;
 
 
+import java.util.List;
+
 public interface IGame {
     void attach(IGameObserver observer);
+    default void attach(IGameObserver observer, List<EventType> events) {
+        attach(observer);
+    };
     default void detach(IGameObserver observer) {
         // Implement if needed
     }
